8766026cd7f7080e6f7664dc6182a0addbde917b
dima <11lione1991@gmail.com>
2019-01-09 15:40:23 +0200

    version 0.1.2

--- js/button.js
+++ js/button.js
@@ -4,18 +4,23 @@ function clickButton(event) {
     var listService = document.getElementsByClassName("list_services")
     var mainServicesImages = document.getElementsByClassName("main_services_images")
 
-    if ( listService[0].style.display === "block" ) {
+    if ( listService[0].style.opacity === "1" ) {
 
         event.target.innerText = "Посмотреть список"
 
-        listService[0].style.display = "none"
-        mainServicesImages[0].style.display = "flex"
+        listService[0].style.opacity = "0"
+        listService[0].style.height = "0"
+        mainServicesImages[0].style.opacity = "1"
+        mainServicesImages[0].style.height = "auto"    
         return
     }
     
     event.target.innerText = "Закрыть список"
 
-    mainServicesImages[0].style.display = "none"
-    listService[0].style.display = "block"
+    mainServicesImages[0].style.opacity = "0"
+    mainServicesImages[0].style.height = "0"
+    
+    listService[0].style.opacity = "1"
+    listService[0].style.height = "auto"
 
 }
